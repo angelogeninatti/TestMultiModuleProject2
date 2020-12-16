86f5044884f53af421177a1be55cf10614fb89a1
Angelo Geninatti <angelo.geninattic@edu.unito.it>
2020-12-16 12:43:34 +0100

    Spring

---
+++ altrospring/src/main/java/com/example/altrospring/AltrospringApplication.java
@@ -0,0 +1,13 @@
+package com.example.altrospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltrospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltrospringApplication.class, args);
+	}
+
+}
---
+++ altrospring/src/test/java/com/example/altrospring/AltrospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.altrospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltrospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
